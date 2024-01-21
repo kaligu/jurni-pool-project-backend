b228287714cd39c5030a71f1214d3496e0d72c9c
Chethiya Kaligu <chethiyakaligu2@gmail.com>
2024-01-22 00:09:43 +0530

    test run done

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,5 +4,6 @@ export const loginUser = async (
     req: express.Request,
     res: express.Response
   ) => {
-
+    console.log(req.body.userData);
+    res.send('POST request to the login');
   }
